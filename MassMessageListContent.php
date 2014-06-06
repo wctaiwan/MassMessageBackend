4417072a477534e72027c445c8b53f7b576d9f98
wctaiwan <wctaiwan@gmail.com>
2014-06-06 04:06:33 -0400

    validation

--- MassMessageListContent.php
+++ MassMessageListContent.php
@@ -5,4 +5,21 @@ class MassMessageListContent extends TextContent {
 	public function __construct( $text ) {
 		parent::__construct( $text, 'MassMessageListContent' );
 	}
+
+	protected function getHtml() {
+
+	}
+
+	protected function validate() {
+		$targets = FormatJson::decode( $this->getNativeData(), true );
+		if ( !$targets ) {
+			return false;
+		}
+		foreach ( $targets as $target ) {
+			if ( !array_key_exists( 'title', $target ) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
