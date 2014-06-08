965bd412372cdf5111d77934642f49bbda187a12
wctaiwan <wctaiwan@gmail.com>
2014-06-08 03:53:38 -0400

    Table display

--- MassMessageListContent.php
+++ MassMessageListContent.php
@@ -20,9 +20,28 @@ class MassMessageListContent extends TextContent {
 	}
 
 	protected function getHtml() {
+		global $wgAllowGlobalMessaging;
+
 		if ( !$this->validate() ) {
 			return '<p class="error">' . wfMessage( 'massmessage-content-invalid' )->text() . '</p>';
 		}
+
+		$targets = $this->getTargets();
+		$rows = array();
+		foreach ( $targets as $target ) {
+			$row = array( $target['title'] );
+			if ( $wgAllowGlobalMessaging ) {
+				$row[] = array_key_exists( 'domain', $target ) ? $target['domain'] : '';
+			}
+			$rows[] = $row;
+		}
+
+		$headers = array( wfMessage( 'massmessage-content-title' )->text() );
+		if ( $wgAllowGlobalMessaging ) {
+			$headers[] = wfMessage( 'massmessage-content-site' )->text();
+		}
+
+		return Xml::buildTable( $rows, array( 'class' => 'wikitable' ), $headers );
 	}
 
 	protected function getTargets() {
