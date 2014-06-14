13e63517785dd1774918ec63b52b30364da02c8b
wctaiwan <wctaiwan@gmail.com>
2014-06-14 03:25:14 -0400

    Override edit link

--- MassMessageBackend.hooks.php
+++ MassMessageBackend.hooks.php
@@ -3,6 +3,14 @@
 class MassMessageBackendHooks {
 
 	public static function onSkinTemplateNavigation( &$sktemplate, &$links ) {
+		$title = $sktemplate->getTitle();
+		if ( $title->getContentModel() === 'MassMessageListContent'
+			&& array_key_exists( 'edit', $links['views'] )
+		) {
+			$links['views']['edit']['href'] = SpecialPage::getTitleFor(
+				'ManageMassMessageList', $title
+			)->getFullUrl();
+		}
 		return true;
 	}
 }
